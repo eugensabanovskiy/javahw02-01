ebb43fe3803697d85df89ea6f06aebc8b7c134ce
Eugen Sabanovskiy <eugensabanovskiy123@gmail.com>
2024-11-17 21:02:37 +0300

    homework remake

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите цену билета: ");
-        int ticketPrice = in.nextInt();
-        int bonusMiles = ticketPrice / 20;
-
-        System.out.printf("Ваши бонусные мили: %d \n", bonusMiles);
-        in.close();
+        int ticketPrice = 13676;
+        int bonusMiles = (ticketPrice / 20);
+        System.out.println("Начисленные мили " + bonusMiles );
     }
 }
+
+
+
+
+
